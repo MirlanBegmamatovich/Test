e5237875d5d0986aa9b7d507f70f9c1b045da43a
User <fastrainygmail.com>
2020-05-11 16:02:05 +0700

    HomeWorkExtra done

--- src/com/company/LessonOne/HomeWorkOne/HomeWorkExtra.java
+++ src/com/company/LessonOne/HomeWorkOne/HomeWorkExtra.java
@@ -2,4 +2,63 @@ package com.company.LessonOne.HomeWorkOne;
 
 public class HomeWorkExtra {
 
+    public static void main  (String[] args){
+
+    byte za=111;
+    short z1=666;
+    int z2=66666;
+    long z3=666666666;
+
+    System.out.println("Первая задача byte для примера было взято -> 73");
+    byte x=0B1001001; // 2-ичная система счислений
+    byte x1=111; // 8-ичная система счислений
+    byte x2=73; // 10-ичная система счислений
+    byte x3=0X49; // 16-ичная система счислений
+    System.out.println("2-ичная система счислений" + x);
+    System.out.println("8-ичная система счислений" +x1);
+    System.out.println("10-ичная система счислений" +x2);
+    System.out.println("16-ичная система счислений" +x3);
+    System.out.println("");
+
+    System.out.println("Вторая задача для short для примера было взято -> 1500");
+    short y=0B10111011100; // 2-ичная система счислений
+    short y1=2734; // 8-ичная система счислений
+    short y2=1500; // 10-ичная система счислений
+    short y3=0X5DC; // 16-ичная система счислений
+    System.out.println("2-ичная система счислений" + y);
+    System.out.println("8-ичная система счислений" + y1);
+    System.out.println("10-ичная система счислений" + y2);
+    System.out.println("16-ичная система счислений" + y3);
+    System.out.println("");
+
+    System.out.println("Третья задача для int для примера было взято -> 150000");
+    int w=0B100100100111110000; // 2-ичная система счислений
+    int w1=444760; // 8-ичная система счислений
+    int w2=1_500_000; // 10-ичная система счислений
+    int w3=0X249F0; // 16-ичная система счислений
+    System.out.println("2-ичная система счислений" + w);
+    System.out.println("8-ичная система счислений" + w1);
+    System.out.println("10-ичная система счислений" + w2);
+    System.out.println("16-ичная система счислений" + w3);
+    System.out.println("");
+
+    System.out.println("Четвертая задача для long для примера было взято -> 1_500_000");
+    long v=0B100110111001001110000111000000; // 2-ичная система счислений
+    long v1=71160700; // 8-ичная система счислений
+    long v2=1_500_000; // 10-ичная система счислений
+    long v3=0xE4E1C0; // 16-ичная система счислений
+    System.out.println("2-ичная система счислений" + v);
+    System.out.println("2-ичная система счислений" + v1);
+    System.out.println("2-ичная система счислений" + v2);
+    System.out.println("2-ичная система счислений" + v3);
+    System.out.println("");
+
+    System.out.println("Использование юникода");
+    System.out.println("Отоброжение знак юаня через юникода");
+    char ch="\uFFE5";
+    System.out.println("Знак юаня через юникод->" + ch);
+
+    }
+
+
 }
